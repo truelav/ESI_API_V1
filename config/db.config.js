6a4b9a9d59d0fc4778a20a308e02534232d354cd
serghei madan <madan.serghei@yahoo.com>
2024-01-12 15:46:43 -0800

    delete git

--- config/db.config.js
+++ config/db.config.js
@@ -4,16 +4,18 @@ import env from "dotenv";
 const dotenv = env.config();
 const devConnection = process.env.MONGODB_STRING;
 const prodConnection = process.env.DB_STRING_PROD;
-const MongoURI = process.env.MONGO_CONNECTION_STRING;
+const MongoURI = process.env.MONGO_URI;
+console.log(MongoURI)
 
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(MongoURI);
+    // mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("Connected to the DB");
   } catch (err) {
     console.log(err);
+    process.exit(1)
   }
 };
 
