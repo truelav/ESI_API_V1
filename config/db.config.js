b561cbc4494109ec49e3dcf047dde8488dfb231d
truelav <madan.serghei@yahoo.com>
2024-03-05 14:27:06 -0800

    db.config - updated db connection uri

--- config/db.config.js
+++ config/db.config.js
@@ -3,12 +3,14 @@ import env from "dotenv";
 
 const dotenv = env.config();
 const devConnection = process.env.MONGODB_STRING;
-const prodConnection = process.env.DB_STRING_PROD;
+console.log(devConnection)
+// const prodConnection = process.env.DB_STRING_PROD;
+
 
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect("mongodb://127.0.0.1:27017/ESI");
+    await mongoose.connect(process.env.MONGODB_STRING);
 
     console.log("Connected to the DB");
   } catch (err) {
